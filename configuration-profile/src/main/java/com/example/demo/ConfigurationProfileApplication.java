8f0134c232f99f11bcfa13e9cb8c06925b80c7e2
softwater <164594770@qq.com>
2017-07-04 17:15:59 +0800

    Profile配置

---
+++ configuration-profile/src/main/java/com/example/demo/ConfigurationProfileApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigurationProfileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigurationProfileApplication.class, args);
+	}
+}
