0ea96b21a77c6aaef87810b68dcc3d8aa7592db9
Z.asan <410595352@qq.com>
2017-07-06 22:14:22 +0800

    websocket

--- websocket/src/main/java/com/example/demo/WebsocketApplication.java
+++ websocket/src/main/java/com/example/demo/WebsocketApplication.java
@@ -8,13 +8,15 @@ import org.springframework.messaging.handler.annotation.SendTo;
 @SpringBootApplication
 public class WebsocketApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WebsocketApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(WebsocketApplication.class, args);
+    }
 
-	@MessageMapping("/welcome")
-	@SendTo("/topic/getResponse") public WebSiteResponse say(WebSiteMessage message) throws InterruptedException {
-		Thread.sleep(3000);
-		return new WebSiteResponse("Hello " + message.getName());
-	}
+    @MessageMapping("/welcome")
+    @SendTo("/topic/getResponse")
+    public WebSiteResponse say(WebSiteMessage message) throws InterruptedException {
+        Thread.sleep(3000);
+        System.out.println(message);
+        return new WebSiteResponse("Hello " + message.getName());
+    }
 }
